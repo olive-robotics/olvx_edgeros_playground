22af7324691fd980e3f45948981943123abec721
Edwin Babaians <edwin.babaians@gmail.com>
2023-09-27 17:33:18 +0200

    Update can_interface_write.py

--- can_interface_write.py
+++ can_interface_write.py
@@ -6,28 +6,34 @@ import struct
 
 CAN_INTERFACE = 'can0'
 
-class CANReadPublisher(Node):
+class CANWriteSubscriber(Node):
     
     def __init__(self):
-        super().__init__('can_read_publisher')
-        self.publisher = self.create_publisher(String, 'can_receive_topic', 10)
+        super().__init__('can_write_subscriber')
+        self.subscription = self.create_subscription(
+            String, 'can_send_topic', self.listener_callback, 10)
+        self.subscription  # prevent unused variable warning
         self.can_socket = socket.socket(socket.AF_CAN, socket.SOCK_RAW, socket.CAN_RAW)
         self.can_socket.bind((CAN_INTERFACE,))
-        timer_period = 0.1  # 100ms
-        self.timer = self.create_timer(timer_period, self.timer_callback)
         
-    def timer_callback(self):
-        can_packet = self.can_socket.recv(16)
-        can_id, can_data = struct.unpack("<I8s", can_packet)
-        message = f"{can_id:X}#{can_data.hex().upper()}"
-        msg = String(data=message)
-        self.publisher.publish(msg)
+    def listener_callback(self, msg):
+        # Here, we'll assume the received string is a hexadecimal representation of the CAN frame.
+        can_id, can_data = self.parse_can_message(msg.data)
+        can_packet = struct.pack("<I8s", can_id, can_data)
+        self.can_socket.send(can_packet)
+        
+    def parse_can_message(self, message):
+        # Simple example to parse a message like '5A1#AABBCCDD'
+        parts = message.split('#')
+        can_id = int(parts[0], 16)
+        can_data = bytes.fromhex(parts[1])
+        return can_id, can_data
 
 def main(args=None):
     rclpy.init(args=args)
-    can_read_publisher = CANReadPublisher()
-    rclpy.spin(can_read_publisher)
-    can_read_publisher.destroy_node()
+    can_write_subscriber = CANWriteSubscriber()
+    rclpy.spin(can_write_subscriber)
+    can_write_subscriber.destroy_node()
     rclpy.shutdown()
 
 if __name__ == '__main__':
